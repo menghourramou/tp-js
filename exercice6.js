4d82cd1f95e242f5bdf894c94f224d44e1f24226
ramouasmaa <ramou.asmaa@yahoo.com>
2020-08-02 16:36:03 +0200

    exercice6

--- exercice6.js
+++ exercice6.js
@@ -1,6 +1,9 @@
+// 6. Créez une fonction nommée "print_something" qui affichera la chaîne de caractères "Something" suivi d’un retour a la ligne, ceci à chaque fois qu’elle sera appelée. 
+
+
 function print_something(){
 
-    console.log("Something");
+    console.log("Something\n");
 
 }
 
