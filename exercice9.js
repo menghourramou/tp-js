12696c50158b1d36c89ab828497030377c4a96e5
ramouasmaa <ramou.asmaa@yahoo.com>
2020-08-02 17:11:28 +0200

    exercice8

---
+++ exercice9.js
@@ -0,0 +1,17 @@
+// 9. Créez une fonction "print_calls" qui ne prend aucun paramètre et qui affiche le nombre de fois qu’elle est appelée
+
+a = 0;
+
+function print_calls(){
+
+    a += 1;
+
+    console.log(a);
+
+}
+
+print_calls();
+print_calls();
+print_calls();
+print_calls();
+
